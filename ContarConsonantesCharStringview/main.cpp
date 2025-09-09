5e638a171b77ae2ac274a260b96acd42ce9b4b0e
Raymond londoño <raymond_5400@hotmail.com>
2025-09-09 08:45:04 -0500

    Refactorizando codigo

--- ContarConsonantesCharStringview/main.cpp
+++ ContarConsonantesCharStringview/main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <cctype>
 
 
 void contadorConsonantes(std::string_view sv);
 
+void contadorConsonantesChar(char* fr);
+
 int main()
 {
 
-	std::string_view vista{ "Hola como Estas, Siempre bien. Cierto?" };
+	std::string_view vista{ "Hola como Estas, Siempre bien. Cierto? 1111 09097" };
 
-	contadorConsonantes(vista);
+	//contadorConsonantes(vista);
+
+	char frase[]{ "Hola, como estas" };
+
+	std::cout << std::endl;
+
+	contadorConsonantesChar(frase);
+
+	//contadorConsonantes(frase);
 
 	return 0;
 }
@@ -24,4 +35,24 @@ void contadorConsonantes(std::string_view sv)
 			c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u'; });
 
 	std::cout << "Contador " << contador;
+}
+
+void contadorConsonantesChar(char* fr)
+{
+	//por defecto se hace false
+	std::size_t contador{};
+
+	for (std::size_t i{}; fr[i] != '\0'; ++i)
+	{
+	
+			char c = static_cast<char>(std::tolower(static_cast<unsigned char>(fr[i])));
+		     
+			if (std::isalpha(static_cast<unsigned char>(c)) && c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u');
+			++contador;
+
+	
+	}
+
+	std::cout << "\nContador de consonante metodo void " << contador;
+
 }
